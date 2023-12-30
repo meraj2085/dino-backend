ff6790b861771b31c03334cd464d3635def17104
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2023-12-30 09:54:28 +0600

    leave interface and model created

---
+++ src/app/modules/leave/leave.interface.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { Model } from 'mongoose';
+
+export type ILeave = {
+  user_id: string;
+  from_date: string;
+  to_date: string;
+  no_of_days: string;
+  reason: string;
+  organization_id: string;
+  status: string;
+};
+
+export type LeaveModel = Model<ILeave, Record<string, unknown>>;
+
+export type ILeaveFilters = {
+  searchTerm?: string;
+  email?: string;
+};
