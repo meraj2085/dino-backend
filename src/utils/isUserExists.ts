591a7e4e7e448bb404665f56c2b74ead0b894b20
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2024-10-14 10:50:01 +0600

    Login API changed

--- src/utils/isUserExists.ts
+++ src/utils/isUserExists.ts
@@ -3,6 +3,6 @@
 export const isUserExist = async function (office_email: string, UserDb: any) {
   return await UserDb.findOne(
     { office_email, status: 'Active' },
-    { _id: 1, password: 1, user_type: 1, office_email: 1, organization_id: 1 }
+    { _id: 1, password: 1, user_type: 1, office_email: 1, organization_id: 1, role: 1 }
   ).lean();
 };
