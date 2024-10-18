7d1c62fda3dade844b444fe8e6116b974b1eaf7c
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2024-10-18 20:20:11 +0600

    Updated user validation

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -17,10 +17,6 @@ const addUserZodSchema = z.object({
   date_of_joining: z.string({
     required_error: 'Date Of Joining is Required',
   }),
-  department: z.enum(
-    ['HR', 'IT', 'Finance', 'Marketing', 'Sales', 'Operations'],
-    { required_error: 'Department is Required' }
-  ),
   flat_number: z.string().optional(),
   building_name: z.string().optional(),
   street: z.string().optional(),
@@ -35,41 +31,10 @@ const addUserZodSchema = z.object({
   bank_name: z.string({ required_error: 'Bank Name is Required' }),
   account_number: z.string({ required_error: 'Account Number is Required' }),
   branch_name: z.string().optional(),
-  designation: z.enum(
-    [
-      'Software Engineer',
-      'Senior Software Engineer',
-      'Team Lead',
-      'Project Manager',
-      'Product Manager',
-      'CEO',
-      'CTO',
-      'COO',
-      'CFO',
-      'HR Manager',
-      'HR Executive',
-      'HR Intern',
-      'Marketing Manager',
-      'Marketing Executive',
-      'Marketing Intern',
-      'Sales Manager',
-      'Sales Executive',
-      'Sales Intern',
-      'Finance Manager',
-      'Finance Executive',
-      'Finance Intern',
-      'Operations Manager',
-      'Operations Executive',
-      'Operations Intern',
-    ],
-    { required_error: 'Designation is Required' }
-  ),
-  team: z.enum(['IT', 'Finance', 'Marketing', 'Sales', 'Operations'], {
-    required_error: 'Team is Required',
-  }),
-  role: z.enum(['Employee', 'Manager', 'Developer', 'Designer', 'Tester'], {
-    required_error: 'Role is Required',
-  }),
+  designation: z.string({ required_error: 'Designation is Required' }),
+  team: z.string({ required_error: 'Team is Required' }),
+  role: z.string({ required_error: 'Role is Required' }),
+  department: z.string({ required_error: 'Department is Required' }),
   manager_id: z.string({ required_error: 'ManagerId is Required' }),
   emergency_contact: z.object({
     full_name: z.string({
@@ -105,98 +70,71 @@ const addUserZodSchema = z.object({
   }),
 });
 
-
 const updateUserZodSchema = z.object({
-    organization_id: z.string().optional(),
-    first_name: z.string().optional(),
-    last_name: z.string().optional(),
-    date_of_birth: z.string().optional(),
-    gender: z.string().optional(),
-    employment_status: z
-      .enum(['Contract', 'Intern', 'Temporary', 'Part-time', 'Freelance', 'Full-time'])
-      .optional(),
-    office_email: z.string().optional(),
-    date_of_joining: z.string().optional(),
-    department: z
-      .enum(['HR', 'IT', 'Finance', 'Marketing', 'Sales', 'Operations'])
-      .optional(),
-    flat_number: z.string().optional(),
-    building_name: z.string().optional(),
-    street: z.string().optional(),
-    city: z.string().optional(),
-    landmark: z.string().optional(),
-    country: z.string().optional(),
-    state: z.string().optional(),
-    postal_code: z.string().optional(),
-    phone_number: z.string().optional(),
-    other_phone_number: z.string().optional(),
-    personal_email: z.string().optional(),
-    bank_name: z.string().optional(),
-    account_number: z.string().optional(),
-    branch_name: z.string().optional(),
-    designation: z
-      .enum([
-        'Software Engineer',
-        'Senior Software Engineer',
-        'Team Lead',
-        'Project Manager',
-        'Product Manager',
-        'CEO',
-        'CTO',
-        'COO',
-        'CFO',
-        'HR Manager',
-        'HR Executive',
-        'HR Intern',
-        'Marketing Manager',
-        'Marketing Executive',
-        'Marketing Intern',
-        'Sales Manager',
-        'Sales Executive',
-        'Sales Intern',
-        'Finance Manager',
-        'Finance Executive',
-        'Finance Intern',
-        'Operations Manager',
-        'Operations Executive',
-        'Operations Intern',
-      ])
-      .optional(),
-    team: z
-      .enum(['IT', 'Finance', 'Marketing', 'Sales', 'Operations'])
-      .optional(),
-    role: z
-      .enum(['Employee', 'Manager', 'Developer', 'Designer', 'Tester'])
-      .optional(),
-    manager_id: z.string().optional(),
-    emergency_contact: z
-      .object({
-        full_name: z.string().optional(),
-        phone_number: z.string().optional(),
-        email: z.string().optional(),
-        relationship: z.string().optional(),
-      })
-      .optional(),
-    contract_date: z.string().optional(),
-    user_type: z
-      .enum(['super_admin', 'admin', 'employee'])
-      .default('employee')
-      .optional(),
-    password: z.string().default('Dino-123').optional(),
-    profile_picture: z.string().optional(),
-    status: z
-      .enum(['Deleted', 'Disabled', 'Active'])
-      .default('Active')
-      .optional(),
-    is_password_reset: z.boolean().default(false).optional(),
-    salaryDetails: z
-      .object({
-        basic_salary: z.string().optional(),
-        total_allowance: z.string().optional(),
-        annual_bonus: z.string().optional(),
-        total_ctc: z.string().optional(),
-      })
-      .optional(),
+  organization_id: z.string().optional(),
+  first_name: z.string().optional(),
+  last_name: z.string().optional(),
+  date_of_birth: z.string().optional(),
+  gender: z.string().optional(),
+  employment_status: z
+    .enum([
+      'Contract',
+      'Intern',
+      'Temporary',
+      'Part-time',
+      'Freelance',
+      'Full-time',
+    ])
+    .optional(),
+  office_email: z.string().optional(),
+  date_of_joining: z.string().optional(),
+  flat_number: z.string().optional(),
+  building_name: z.string().optional(),
+  street: z.string().optional(),
+  city: z.string().optional(),
+  landmark: z.string().optional(),
+  country: z.string().optional(),
+  state: z.string().optional(),
+  postal_code: z.string().optional(),
+  phone_number: z.string().optional(),
+  other_phone_number: z.string().optional(),
+  personal_email: z.string().optional(),
+  bank_name: z.string().optional(),
+  account_number: z.string().optional(),
+  branch_name: z.string().optional(),
+  designation: z.string().optional(),
+  team: z.string().optional(),
+  department: z.string().optional(),
+  role: z.string().optional(),
+  manager_id: z.string().optional(),
+  emergency_contact: z
+    .object({
+      full_name: z.string().optional(),
+      phone_number: z.string().optional(),
+      email: z.string().optional(),
+      relationship: z.string().optional(),
+    })
+    .optional(),
+  contract_date: z.string().optional(),
+  user_type: z
+    .enum(['super_admin', 'admin', 'employee'])
+    .default('employee')
+    .optional(),
+  password: z.string().default('Dino-123').optional(),
+  profile_picture: z.string().optional(),
+  status: z
+    .enum(['Deleted', 'Disabled', 'Active'])
+    .default('Active')
+    .optional(),
+  is_password_reset: z.boolean().default(false).optional(),
+  salaryDetails: z
+    .object({
+      basic_salary: z.string().optional(),
+      total_allowance: z.string().optional(),
+      annual_bonus: z.string().optional(),
+      total_ctc: z.string().optional(),
+    })
+    .optional(),
 });
 
 export const UserValidation = {
